ef9b66f6e69ceeef81ddf431aee062d352fc4c63
Xueli Jin <xueli.jin@sanoma.com>
2018-02-28 17:08:53 +0200

    Basic router to home and gallery, plus adding angular material

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -4,9 +4,10 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 import { GalleryComponent } from '../gallery/gallery.component';
+import { AppComponent } from '../app.component';
 
 export const routes: Routes = [
-  // { path: 'home',  component: HomeComponent },
+  { path: 'home',  component: AppComponent },
   { path: 'aboutus',     component: AboutComponent },
   { path: 'contactus',     component: ContactComponent },
   { path: 'gallery/:id',     component: GalleryComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import 'hammerjs';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
